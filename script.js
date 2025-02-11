bc963f94e07929ef5adc5512045d5aaf36ef50c5
Jyotroop Singh <jyotroopsingh@gmail.com>
2025-02-11 14:50:56 +0530

    added loading animation

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const promptInput = promptForm.querySelector(".prompt-input");
 
 let userMessage = "";
 
-const createMsgElement = (content, className) => {
+// Function to create message element
+const createMsgElement = (content, ...classes) => {
     const div = document.createElement("div");
-    div.classList.add("message", className);
+    div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
 }
@@ -16,6 +17,8 @@ const handleFormSubmit = (e) => {
     e.preventDefault();
     userMessage = promptInput.value.trim();
     if(!userMessage) return;
+
+    promptInput.value = "";
 
     // Generate user message HTML and add in the chats container
     const userMsgHTML = `<p class="message-text"></p>`
@@ -27,7 +30,7 @@ const handleFormSubmit = (e) => {
     setTimeout(() => {
         // Generate bot message HTML and add in the chats container in 600ms
         const botMsgHTML = `<img src="assets/gemini-chatbot-logo.svg" class="avatar"><p class="message-text">Just a sec...</p>`
-        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message")
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading")
         chatsContainer.appendChild(botMsgDiv);
  }, 600);
  
