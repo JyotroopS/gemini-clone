470e6d540be6d6a2f857b0effc3d0652a4e169cf
Jyotroop Singh <jyotroopsingh@gmail.com>
2025-02-11 14:32:56 +0530

    added JS file

---
+++ script.js
@@ -0,0 +1,37 @@
+const chatsContainer = document.querySelector(".chats-container");
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = promptForm.querySelector(".prompt-input");
+
+let userMessage = "";
+
+const createMsgElement = (content, className) => {
+    const div = document.createElement("div");
+    div.classList.add("message", className);
+    div.innerHTML = content;
+    return div;
+}
+
+// Handle the form submission
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+    userMessage = promptInput.value.trim();
+    if(!userMessage) return;
+
+    // Generate user message HTML and add in the chats container
+    const userMsgHTML = `<p class="message-text"></p>`
+    const userMsgDiv = createMsgElement(userMsgHTML, "user-message")
+
+    userMsgDiv.querySelector(".message-text").textContent = userMessage;
+    chatsContainer.appendChild(userMsgDiv);
+
+    setTimeout(() => {
+        // Generate bot message HTML and add in the chats container in 600ms
+        const botMsgHTML = `<img src="assets/gemini-chatbot-logo.svg" class="avatar"><p class="message-text">Just a sec...</p>`
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message")
+        chatsContainer.appendChild(botMsgDiv);
+ }, 600);
+ 
+}
+
+
+promptForm.addEventListener("submit", handleFormSubmit);
