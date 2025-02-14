13843f881ab6fa120da52189f0bac973f9caccd7
Jyotroop Singh <jyotroopsingh@gmail.com>
2025-02-14 17:51:25 +0530

    Gemini API implemented and basic UI changes

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const container = document.querySelector(".container");
 const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
+// API Setup
+const API_KEY = "AIzaSyBfQ7n3-HyiV7CxSHK_AfqVHicLwDZU9dg";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+
 let userMessage = "";
+const chatHistory = [];
 
 // Function to create message element
 const createMsgElement = (content, ...classes) => {
@@ -10,6 +16,57 @@ const createMsgElement = (content, ...classes) => {
     div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
+}
+
+// Function for scrollToBottom
+const scrollToBottom = () => container.scrollTo({ top: container.scrollHeight, behavior: "smooth"});
+
+// Simulate typing effect for bot response
+const typingEffect = (text, textElement, botMsgDiv) => {
+    textElement.textContent = "";
+    const words = text.split(" ");
+    let wordIndex = 0;
+
+    // Set an interval to type each word
+    const typingInterval = setInterval(() => {
+        if(wordIndex < words.length) {
+            textElement.textContent += (wordIndex === 0 ? " " : " ") + words[wordIndex++];
+            scrollToBottom();
+        } else {
+            clearInterval(typingInterval);
+            botMsgDiv.classList.remove("loading");
+        }
+    }, 40);
+}
+
+// Make the API call and generate the bot's reponse
+const generateResponse = async (botMsgDiv) => {
+    const textElement = botMsgDiv.querySelector(".message-text");
+
+    // Add user message to chat history
+    chatHistory.push({
+        role: "user",
+        parts: [{ text: userMessage}]
+    });
+
+    try {
+        // Send the chat history to API to get a response
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ contents: chatHistory})
+        });
+
+        const data = await response.json();
+        if(!response.ok) throw new Error(data.error.message);
+
+        // Process the reponse text and display it with typing effect
+        const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
+        typingEffect(responseText, textElement, botMsgDiv);
+        chatHistory.push({role: "model", parts: [{text: responseText}]});
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Handle the form submission
@@ -26,12 +83,15 @@ const handleFormSubmit = (e) => {
 
     userMsgDiv.querySelector(".message-text").textContent = userMessage;
     chatsContainer.appendChild(userMsgDiv);
+    scrollToBottom();
 
     setTimeout(() => {
-        // Generate bot message HTML and add in the chats container in 600ms
+        // Generate bot message HTML and add in the chats container after 600ms
         const botMsgHTML = `<img src="assets/gemini-chatbot-logo.svg" class="avatar"><p class="message-text">Just a sec...</p>`
         const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading")
         chatsContainer.appendChild(botMsgDiv);
+        scrollToBottom();
+        generateResponse(botMsgDiv);
  }, 600);
  
 }
